13e5bdb3bb853f4ac007159f53acd04d7895f503
Rodrigo Soares <rodrigo.soares@navi.com.br>
2020-03-06 20:15:43 -0300

    update socios

---
+++ documents/partners.js
@@ -0,0 +1,149 @@
+var partners_pt = {
+    felipecampos: {
+        linkedin: "https://www.linkedin.com/in/felipe-campos-navi/",
+        title: "Olá, sou o Felipe Campos.",
+        subtitle: "Sou sócio fundador, CEO e co-CIO da Navi Capital.",
+        body: "Por 9 anos (2009 – 2017) fui responsável pela gestão de renda variável na Kondor Invest, onde fui gestor do fundo Long Short, desde seu início, e do fundo Long Only, desde 2015. Minha carreira começou no Grupo BBM(1999– 2008), onde passei por áreas como análise e gestão.Entre 2008 e 2009, fui co - gestor de ações no Banco Itaú BBA.Sou engenheiro de produção formado pela UFRJ e possuo MBA em Finanças pela FGV."
+    },
+    waldirserafim: {
+        linkedin: "https://www.linkedin.com/in/waldir-serafim-filho-57915120/",
+        title: "Olá, sou o Waldir Serafim.",
+        subtitle: "Sou sócio fundador e co-CIO da Navi Capital.",
+        body: "Por 5 anos (2014 – 2018) fui sócio gestor de renda variável na Kondor Invest. Antes disso, entre 2011 e 2013, fui analista de diversos setores.Iniciei minha carreira na Invest Tech, gestora de fundos de Venture Capital focada em empresas de tecnologia e, posteriormente, passei pela asset do Banco Safra, onde fui analista de empresas por 3 anos (2008 - 2010).Sou economista formado pelo Insper."
+    },
+    matheusamorim: {
+        linkedin: "https://www.linkedin.com/in/matheus-amorim-8795b351/",
+        title: "Olá, sou o Matheus Amorim.",
+        subtitle: "Sou sócio fundador e analista de renda variável da Navi Capital.",
+        body: "Por 2 anos e meio (2016-2018) fui analista de renda variável no setor de energia elétrica, saneamento, óleo e gás e tecnologia na Kondor Invest. Minha carreira começou na Advent International, na equipe de investimentos de Private Equity, em 2013. Entre 2014 e 2015, fui analista de renda variável na 3G Capital, cobrindo os setores elétrico, de concessão rodoviária e de bens de capital. Sou Engenheiro Aeronáutico formado pelo ITA."
+    },
+    jorgedantas: {
+        linkedin: "https://www.linkedin.com/in/jorge-dantas-neto-cfa-618a5497/",
+        title: "Olá, sou o Jorge Dantas.",
+        subtitle: "Sou sócio fundador e analista de renda variável da Navi Capital.",
+        body: "Minha carreira se iniciou na Kondor Invest, onde fui analista de empresas por 7 anos (2012- 2018). Atualmente, sou analista responsável pelos setores financeiro, saúde e shoppings da Navi.Sou economista formado pelo Insper e CFA® Charterholder desde 2018."
+    },
+    luisasouto: {
+        linkedin: "https://www.linkedin.com/in/luisa-carneiro-alves-souto-20569048/",
+        title: "Olá, sou a Luisa Souto.",
+        subtitle: "Sou sócia fundadora e analista de renda variável da Navi Capital.",
+        body: "Por 2 anos (2017 – 2018) fui analista de renda variável no setor de consumo e varejo na Kondor Invest. Iniciei minha carreira no Banco CR2 (2010-2012), onde passei pelas áreas de tesouraria e análise de crédito. Entre 2012 e 2016, fui analista de ações no Banco Brasil Plural, cobrindo os setores elétrico, de concessão rodoviária e de telecom. Sou Economista formada pela PUC-RJ."
+    },
+    gustavolima: {
+        linkedin: "https://www.linkedin.com/in/gustavo-lima-bbb515147/",
+        title: "Olá, sou o Gustavo Lima.",
+        subtitle: "Sou sócio fundador e analista de renda variável da Navi Capital.",
+        body: "Por 2 anos (2017 – 2018) fui analista de renda variável nos setores de papel e celulose, mineração, siderurgia e alimentos na Kondor Invest. Depois de passar pela área de risco da Oceana Investimentos (2010) e pesquisa de mercado no Instituto de Engenharia e Gestão (Grupo Visagio, 2011-2013), ingressei na Tese Investimentos, em 2013. Lá, fiz parte do time de risco, gerencial e backoffice (2013-2015) e, posteriormente, participei da equipe de gestão, focado em trades macro globais (2015-2016). Sou Engenheiro de Produção formado pela UFRJ e possuo mestrado em Macroeconomia e Finanças pela PUC-RJ."
+    },
+    victorcerqueira: {
+        linkedin: "https://www.linkedin.com/in/victor-cerqueira-56483022/",
+        title: "Olá, sou o Victor Cerqueira.",
+        subtitle: "Sou sócio e analista de renda variável da Navi Capital.",
+        body: "Também trabalhei como analista na Studio Investimentos, onde atuei por 4 anos (2014-2018) e na Truxt Investimentos (2018-2020), cobrindo os setores de aviação, locação de veículos, tecnologia, telecomunicações e agribusiness. Sou formado em Economia pelo IBMEC-RJ e pós-graduado em Macroeconomia e Finanças pela PUC-RJ."
+    },
+    raphaelornellas: {
+        linkedin: "https://www.linkedin.com/in/raphael-ornellas-81607949/",
+        title: "Olá, sou o Raphael Ornellas.",
+        subtitle: "Sou o economista-chefe da Navi Capital.",
+        body: "Fui professor de Economia Internacional da PUC-Rio por 2 anos. Posteriormente, atuei como economista sênior do Banco Brasil Plural e fui responsável pela análise da economia brasileira da Garde Asset. Possuo Doutorado em Economia pela PUC-Rio."
+    },
+    marcelladerze: {
+        linkedin: "https://www.linkedin.com/in/marcella-derze-116a2b73/",
+        title: "Olá, sou a Marcella Derze.",
+        subtitle: "Sou sócia e responsável pela área de RI da Navi.",
+        body: "Minha carreira começou na área de Macroeconomia do Bahia Asset Management (2012 – 2016). Posteriormente, migrei para a área de Relação com Investidores e me tornei sócia em 2017. Sou Economista e Mestre em Macroeconomia e Finanças pela PUC-RJ."
+    },
+    gustavoribas: {
+        linkedin: "https://www.linkedin.com/in/gustavo-ribas/",
+        title: "Olá, sou o Gustavo Ribas.",
+        subtitle: "Sou sócio e diretor da Navi.",
+        body: "Por 7 anos (2012-2019) fui executivo do grupo 3G, iniciando como analista de renda variável na 3G Capital e, posteriormente, me tornando membro do Comitê de Investimentos para América Latina. De 2016 a 2019, fui diretor da tesouraria global do Burger King, nos EUA, e head global de real estate. Minha carreira começou no Banco BBM (2005-2010) e, entre 2010 e 2012, fui analista de renda variável no Itaú Asset. Sou economista formado pela UFRJ, advogado pela UERJ e mestre em Economia e Finanças pela FGV, EPGE."
+    },
+    joaoaragon: {
+        linkedin: "https://www.linkedin.com/in/jo%C3%A3o-paulo-de-aragon-frm-a8558649/",
+        title: "Olá, sou o João Aragon.",
+        subtitle: "Sou sócio fundador e COO da Navi.",
+        body: "Por 4 anos (2012 – 2016) fui sócio e responsável pela área de operações e risco da Tese Investimentos. Iniciei minha carreira na RiskControl (2007-2009), na época uma empresa do Grupo BBM, passando pelas áreas de desenvolvimento e suporte. Entre 2009 e 2012, fui analista de risco e operações na Pollux Capital. Sou formado em engenharia elétrica na PUC-RIO, possuo mestrado em Finanças pela FGV e certificação de Financial Risk Manager."
+    },
+    marceloaraujo: {
+        linkedin: "https://www.linkedin.com/in/marcelo-freitas-de-araujo-cfa-215151b3/",
+        title: "Olá, sou o Marcelo Araujo.",
+        subtitle: "Sou sócio e coordenador de operações da Navi.",
+        body: "Atuei no backoffice do Banco BOCOM BBM por mais de 3 anos, onde assumi a coordenação das operações de clientes, incluindo a gestora do grupo que é focada em fundos exclusivos. Anteriormente, atuei nas áreas de M&A pela EBAM (empresa do grupo GP) e em inteligência de mercado pelo Instituto de Engenharia de Gestão. Sou Engenheiro de Produção formado pela UFRJ e CFA® Charterholder desde 2019."
+    },
+};
+
+var partners_en = {
+    felipecampos: {
+        linkedin: "https://www.linkedin.com/in/felipe-campos-navi/",
+        title: "Hello, I am Felipe Campos.",
+        subtitle: "I’m a founding partner, CEO and co-CIO of Navi Capital.",
+        body: "For nine years (2009-2017), I was the partner responsible for equities at Kondor Invest, where I was the manager of the Long Short Fund since its inception and the Long Only Fund since 2015. My career started in the BBM Group (1999-2008), passing through several areas first as an analyst and then equities PM.  Between 2008 and 2009, I was co-PM at Banco Itaú BBA.  I am an Engineer from UFRJ and I hold an MBA in Finance from FGV. "
+    },
+    waldirserafim: {
+        linkedin: "https://www.linkedin.com/in/waldir-serafim-filho-57915120/",
+        title: "Hello, I am Waldir Serafim. ",
+        subtitle: "I’m a founding partner and co-CIO of Navi Capital.",
+        body: "For five years, I was a partner of the equities branch at Kondor Invest (2014-2018) and before that I was an analyst in several sectors from 2011 to 2013.  My career started at Invest Tech, a fund manager of Venture Capital focused on technology companies.  Besides that, I was an analyst for 3 years (2008-2010) at Banco Safra. I am an Economist from Insper."
+    },
+    matheusamorim: {
+        linkedin: "https://www.linkedin.com/in/matheus-amorim-8795b351/",
+        title: "Hello, I am Matheus Amorim.",
+        subtitle: "I’m a founding partner and equity analyst at Navi Capital.",
+        body: "For two and a half years (2016-2018), I was an equity analyst in the utilities, oil and gas, and technology sectors at Kondor Invest. My career started at Advent International (2013) as a trainee in the Private Equity investment team. Between 2014 and 2015, I was an equity analyst at 3G Capital, covering the electric, road concession and capital goods sectors. I am an Aeronautical Engineer formed by ITA. "
+    },
+    jorgedantas: {
+        linkedin: "https://www.linkedin.com/in/jorge-dantas-neto-cfa-618a5497/",
+        title: "Hello, I am Jorge Dantas.",
+        subtitle: "I’m a founding partner and equity analyst at Navi Capital.",
+        body: "My career started at Kondor Invest, where I was an equity analyst for seven years (2012-2018), responsible for financial, health and shopping malls.  I am an Economist graduated from Insper and CFA® Charterholder since 2018."
+    },
+    luisasouto: {
+        linkedin: "https://www.linkedin.com/in/luisa-carneiro-alves-souto-20569048/",
+        title: "Hello, I am Luisa Souto.",
+        subtitle: "I’m a founding partner and equity analyst at Navi Capital.",
+        body: "I was an equity analyst in the consumer and retail sector at Kondor Invest from 2017 to 2018. My career started at Banco CR2 (2010-2012), going through the areas of treasury and credit analysis. Between 2012 and 2016, I was an equity analyst at Banco Brasil Plural, covering the electric, road concession and telecom sectors. I am an Economist graduated from PUC-RJ. "
+    },
+    gustavolima: {
+        linkedin: "https://www.linkedin.com/in/gustavo-lima-bbb515147/",
+        title: "Hello, I am Gustavo Lima. ",
+        subtitle: "I’m a founding partner and equity analyst at Navi Capital. ",
+        body: "I was an equity analyst in the paper and pulp, mining, steel and food sectors at Kondor Invest (2017-2018). After attending the risk area of Oceana Investimentos (2010) and market research at the Institute of Engineering and Management (Visagio Group, 2011-2013), I joined Tese Investimentos in 2013. In Tese, I was part of the risk management (2013-2015) and, after, participated in the management team, focused on macro-global trades (2015-2016). I am an Industrial Engineer graduated from UFRJ and I hold a master’s degree in Macroeconomics and Finance from PUC-RJ. "
+    },
+    victorcerqueira: {
+        linkedin: "https://www.linkedin.com/in/victor-cerqueira-56483022/",
+        title: "Hello, I am Victor Cerqueira.",
+        subtitle: "I’m a partner and equity analyst at Navi Capital. ",
+        body: "I worked at Studio Investimentos (2014-2018) and Truxt Investimentos (2018-2020), where I was responsible for aviation, vehicle leasing, technology, telecom and agribusiness sectors. I am an Economist graduated from IBMEC-RJ and postgraduate in Macroeconomics and Finance from PUC-RJ."
+    },
+    raphaelornellas: {
+        linkedin: "https://www.linkedin.com/in/raphael-ornellas-81607949/",
+        title: "Hello, I am Raphael Ornellas.",
+        subtitle: "I’m an Economist at Navi Capital.",
+        body: "For two years, I was a professor of International Economics at PUC-Rio. Later, I served as senior economist at Banco Brasil Plural and Garde Asset, responsible for analyzing the Brazilian economy. I hold a Ph.D. in Economics from PUC-Rio. "
+    },
+    marcelladerze: {
+        linkedin: "https://www.linkedin.com/in/marcella-derze-116a2b73/",
+        title: "Hello, I am Marcella Derze.",
+        subtitle: "I’m responsible for the IR area at Navi.",
+        body: "My career started as a macroeconomics analyst at BBM Investments (current Bahia Asset – from 2012 - 2016) and I migrated to Investor Relations and became a Partner in 2017. I left the institution in 2019 when I took over the current activities. I am an Economist and Master in Macroeconomics and Finance by PUC-RJ."
+    },
+    gustavoribas: {
+        linkedin: "https://www.linkedin.com/in/gustavo-ribas/",
+        title: "Hello, I am Gustavo Ribas.",
+        subtitle: "I’m a partner, member of the Investment Committee and the Deputy CEO of Navi.",
+        body: "For the last seven years, I was an executive of 3G Capital, first as an investment professional and member of 3G Brazil Investment Committee, and later as the Global Head of Real Estate at Restaurant Brands International, a 3G portfolio company and parent of Burger King, Tim Hortons and Popeyes. From 2010-2012, I was an equity research associate in Itaú Asset Management’s hedge fund group. My career started at BBM Bank in the derivatives and high yield debt financing department. I hold an Economics degree from UFRJ, a Law degree from UERJ and a master’s in finance from FGV."
+    },
+    joaoaragon: {
+        linkedin: "https://www.linkedin.com/in/jo%C3%A3o-paulo-de-aragon-frm-a8558649/",
+        title: "Hello, I am João Aragon.",
+        subtitle: "I’m a Founding partner and COO of Navi Capital.",
+        body: "For four years (2012-2016), I was partner and responsible for the operations and risk areas of Tese Investimentos. My career started at RiskControl (2007-2009), a BBM Group company, going through development and support areas. Between 2009 and 2012, I was a risk analyst and operations at Pollux Capital. I hold a degree in electrical engineering from PUC-RIO, a master's degree in Finance from FGV and a certification from Financial Risk Manager."
+    },
+    marceloaraujo: {
+        linkedin: "https://www.linkedin.com/in/marcelo-freitas-de-araujo-cfa-215151b3/",
+        title: "Hello, I am Marcelo Araujo.",
+        subtitle: "I’m a Coordinator of Navi Capital operations.",
+        body: "For more than 3 years, I worked in the back office of Banco BOCOM BBM where I coordinated client operations, including the group manager focused on exclusive funds. Previously I worked in the areas of M&A by EBAM (company of the GP group) and in market intelligence by the Institute of Management Engineering. I am a Production Engineer graduated from UFRJ and CFA® Charterholder since 2018."
+    },
+};
